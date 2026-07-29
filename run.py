a8206b6e5816aa38149451961505bdc9b700e5a1
Vivaan Suri <vivaan@froresystems.com>
2026-07-29 16:02:32 -0700

    random change

--- run.py
+++ run.py
@@ -13,3 +13,5 @@ import uvicorn
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", "8000"))
     uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
+
+# random change
